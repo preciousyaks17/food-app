eddebf155fff79c5a6fe85f34f55ded3557f6de1
preciousyaks17 <johndoe@example.com>
2024-07-21 00:39:33 +0100

    implemented the handle submit form for the share meal form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,7 @@
-"use client";
 import { shareMeal } from "@/lib/actions";
 import classes from "./page.module.css";
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
   return (
@@ -43,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
